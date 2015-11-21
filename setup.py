5555af82c2cf872cf63e53a01320dcc2064137fc
Alex Van Boxel <alex@vanboxel.be>
2015-11-21 14:04:50 +0100

    Added setup.py for package installation

---
+++ setup.py
@@ -0,0 +1,15 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name="luigi-gcloud",
+    version="0.0.1",
+    description="Extends Luigi with support for the Google Cloud Platform.",
+    url="https://github.com/alexvanboxel/luigiext-gcloud",
+    author="Alex Van Boxel",
+    author_email="alex@vanboxel.be",
+    packages=['luigi_gcloud'],
+    install_requires=["luigi", "google-api-python-client"]
+)
