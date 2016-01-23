33417a5c445c43587e45ef29c33cbddb13ef7e4f
Alex Van Boxel <alex@vanboxel.be>
2016-01-23 14:35:46 +0100

    Give the logger it's own thread closes #16

--- luigi_gcloud/log.py
+++ luigi_gcloud/log.py
@@ -1,6 +1,21 @@
+import Queue
 import logging
+import thread
 
 from luigi_gcloud.gcore import get_default_client
+
+
+def logger(logging_api, queue):
+    while True:
+        entry = queue.get()
+        logging_api.entries().write(
+                body={
+                    "entries": [
+                        entry
+                    ]
+                }
+        ).execute()
+        queue.task_done()
 
 
 class CloudLoggingHandler(logging.Handler):
@@ -11,24 +26,19 @@ class CloudLoggingHandler(logging.Handler):
         self.project_id = self.project_id = client.project_id()
         http = client.http_authorized()
         self.logging_api = client.logging_api(http)
+        self.queue = Queue.Queue()
+        thread.start_new_thread(logger, (self.logging_api, self.queue))
 
     def emit(self, record):
         # print(str(vars(record)))
-        self.logging_api.entries().write(
-                body={
-                    "entries": [
-                        {
-                            "severity": record.levelname,
-                            "jsonPayload": {
-                                "logger": record.name,
-                                "module": record.module,
-                                "message": record.getMessage()
-                            },
-                            "logName": "projects/" + self.project_id + "/logs/" + self.name,
-                            "resource": {
-                                "type": "global",
-                            }
-                        }
-                    ]
-                }
-        ).execute()
+        self.queue.put({
+            "severity": record.levelname,
+            "jsonPayload": {
+                "logger": record.name,
+                "module": record.module,
+                "message": record.getMessage()
+            },
+            "logName": "projects/" + self.project_id + "/logs/" + self.name,
+            "resource": {
+                "type": "global",
+            }})
